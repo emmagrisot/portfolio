d83728688403ad011c2b91ce6d90a500594efa28
Emma <emma.grisot@gmail.com>
2015-04-13 09:01:17 +0200

    changement nom de domaine

--- js/localscroll/jquery.scrollTo.js
+++ js/localscroll/jquery.scrollTo.js
@@ -81,7 +81,7 @@
 
 			var doc = (elem.contentWindow || elem).document || elem.ownerDocument || elem;
 			
-			return $.support.safari || doc.compatMode == 'BackCompat' ?
+			return $.browser.safari || doc.compatMode == 'BackCompat' ?
 				doc.body : 
 				doc.documentElement;
 		});
